b0570d9ab163266d615fd45680489959f28b7eb4
Shaikah98 <SHAIKAH98@outlook.sa>
2026-08-14 23:06:19 +0300

    Enable CORS support in Flask app

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import joblib
 import os
 
 app = Flask(__name__)
-cors (app)
+CORS(app)
 
 # Load trained machine failure model
 MODEL_PATH = os.path.join(
